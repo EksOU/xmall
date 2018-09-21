9029f54b6ba16920ea488c7b03e832ce8dbb8f4e
Eks OU <eks999@dingtalk.com>
2018-09-21 13:08:54 +0800

    dubbo整合（三）

--- mall-dao/src/main/java/life/eks/xmall/dao/config/DaoConfiguration.java
+++ mall-dao/src/main/java/life/eks/xmall/dao/config/DaoConfiguration.java
@@ -1,12 +1,50 @@
 package life.eks.xmall.dao.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author Eks OU
  */
 @SpringBootApplication
-@MapperScan("life.eks.xmall")
+@MapperScan("life.eks.xmall.dao")
 public class DaoConfiguration {
+
+    @Bean(name = "druidProp")
+    public PropertiesFactoryBean getDruidPropFactoryBean() {
+        PropertiesFactoryBean factoryBean = new PropertiesFactoryBean();
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource resource = resolver.getResource("classpath:druid.properties");
+        factoryBean.setLocation(resource);
+        return factoryBean;
+    }
+
+    @Bean
+    public DataSource getDataSource(Properties druidProp) {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.configFromPropety(druidProp);
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean getSqlSessionFactoryBean(DataSource dataSource) throws IOException {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("classpath:life/eks/xmall/dao/mapper/*.xml");
+        factoryBean.setMapperLocations(resources);
+        factoryBean.setTypeAliasesPackage("life.eks.xmall.pojo");
+        return factoryBean;
+    }
 }
--- mall-goods-service/src/main/java/life/eks/xmall/service/ServiceGoodsBoot.java
+++ mall-goods-service/src/main/java/life/eks/xmall/service/ServiceGoodsBoot.java
@@ -1,17 +1,16 @@
 package life.eks.xmall.service;
 
-import org.mybatis.spring.annotation.MapperScan;
+import life.eks.xmall.dao.config.DaoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Eks OU
  */
 @SpringBootApplication
-//@Import(DaoConfiguration.class)
-@MapperScan("life.eks.xmall")
+@Import(DaoConfiguration.class)
 public class ServiceGoodsBoot {
-
     public static void main(String[] args) {
         SpringApplication.run(ServiceGoodsBoot.class, args);
     }
